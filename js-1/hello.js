1f45a4414382b9cc7c9c773769705e340d1cbe0f
Alfonso <c-walk5@gmail.com>
2023-05-10 20:00:36 -0500

    commit

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+
+function hola(nombre) {
+    return "Hola " + pedro + "!";
+  }
+  
+  console.log(hola("Pedro")); // "Hola Pedro!"
+  console.log(hola("Juan")); // "Hola Juan!"
+  console.log(hola("")); // "Hola !"
